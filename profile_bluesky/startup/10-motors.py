171c2692af8e8abca04653dce25b20fc84165ddf
jemian <prjemian@gmail.com>
2018-01-17 13:27:19 -0600

    support default `wa` command

--- profile_bluesky/startup/10-motors.py
+++ profile_bluesky/startup/10-motors.py
@@ -20,3 +20,5 @@ m5 = EpicsMotor('3idc:m5', name='m5')	# A1_Theta
 m6 = EpicsMotor('3idc:m6', name='m6')	# A1_Chi
 m7 = EpicsMotor('3idc:m7', name='m7')	# A3_Theta
 m8 = EpicsMotor('3idc:m8', name='m8')	# A3_Chi
+
+append_wa_motor_list(m1, m2, m3, m4, m5, m6, m7, m8)
