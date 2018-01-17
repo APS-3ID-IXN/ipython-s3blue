ccb1e230f9712d3b5948ec1366249d20c859ee86
jemian <prjemian@gmail.com>
2018-01-17 13:46:44 -0600

    prefer original name

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -10,8 +10,8 @@ from ophyd import PVPositioner, PVPositionerPC
 # Set up default complex devices
 
 class MotorDialValues(Device):
-    value = Cpt(EpicsSignalRO, ".DRBV")
-    setpoint = Cpt(EpicsSignal, ".DVAL")
+    value = Component(EpicsSignalRO, ".DRBV")
+    setpoint = Component(EpicsSignal, ".DVAL")
 
 class MyEpicsMotorWithDial(EpicsMotor):
-    dial = Cpt(MotorDialValues, "")
+    dial = Component(MotorDialValues, "")
