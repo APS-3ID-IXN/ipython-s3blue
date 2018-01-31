69fca0cec2b830f52bf54299915e545552869b2d
jemian <prjemian@gmail.com>
2018-01-31 17:27:31 -0600

    remove unused

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -1,11 +1,10 @@
 print(__file__)
 
+"""Set up default complex devices"""
+
 
 from ophyd import Component, Device, DeviceStatus
 from ophyd import EpicsMotor, EpicsScaler
 from ophyd import EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV
 from ophyd import PVPositioner, PVPositionerPC
 from APS_BlueSky_tools.devices import userCalcsDevice
-
-
-# Set up default complex devices
