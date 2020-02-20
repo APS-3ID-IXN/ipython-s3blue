d861f833ee6b589237a630a64ae0b5fb57b5e0e3
Pete Jemian <prjemian@gmail.com>
2020-02-20 13:01:57 -0600

    AHA #8 there's a local modification!

--- profile_bluesky/startup/instrument/devices/energy.py
+++ profile_bluesky/startup/instrument/devices/energy.py
@@ -10,6 +10,27 @@ __all__ = [
 from ..session_logs import logger
 logger.info(__file__)
 
-from apstools.devices import SscanRecord
+import apstools.devices
+
+class SscanRecord(apstools.devices.sscanRecord):
+    
+    def set(self, value, **kwargs):
+        """interface to use bps.mv()"""
+        if value != 1:
+            return
+
+        working_status = DeviceStatus(self)
+        started = False
+
+        def exsc_cb(value, timestamp, **kwargs):
+            value = int(value)
+            if started and value == 0:
+                working_status._finished()
+        
+        self.exsc.subscribe(exsc_cb)
+        self.exsc.set(1)
+        started = True
+        return working_status
+
 
 energy_scan = SscanRecord("3idb:scan1", name="energy_scan")
