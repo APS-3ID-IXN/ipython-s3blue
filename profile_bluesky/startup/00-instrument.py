75a98d93c810e5da19d4609211c4d88e2e9b7f5b
Pete Jemian <prjemian@gmail.com>
2020-02-20 12:12:41 -0600

    MNT #8 framework

---
+++ profile_bluesky/startup/00-instrument.py
@@ -0,0 +1,9 @@
+
+"""
+start bluesky in IPython session for 3-ID
+"""
+
+from instrument.collection import *
+
+# show_ophyd_symbols()
+# print_RE_md(printing=False)
---
+++ profile_bluesky/startup/instrument/collection.py
@@ -0,0 +1,17 @@
+
+"""
+configure for data collection in a console session
+"""
+
+from .session_logs import logger
+logger.info(__file__)
+
+from . import mpl
+
+logger.info("bluesky framework")
+
+from .framework import *
+from .devices import *
+from .plans import *
+
+from apstools.utils import *
